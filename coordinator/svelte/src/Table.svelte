e17517580786bc8b3ad8b33e47725d7c87787098
Juan José Blanco <juan.blanco03@uptc.edu.co>
2021-04-20 22:59:40 -0500

    Added button for delete server and table log. First version finished

---
+++ coordinator/svelte/src/Table.svelte
@@ -0,0 +1,50 @@
+<script>
+</script>
+
+<div class="overflow-auto h-72">
+  <div class="flex items-center justify-center">
+    <div class="">
+      <div class="rounded-lg">
+        <table class="table-auto">
+          <thead>
+            <tr
+              class="bg-gray-200 text-gray-600 uppercase text-base leading-normal"
+            >
+              <th class="py-3 px-6 text-left">Hora</th>
+              <th class="py-3 px-6 text-center">Accion</th>
+            </tr>
+          </thead>
+          <tbody class="text-gray-600 text-base font-light">
+            <tr class="border-b border-gray-200 bg-gray-50 hover:bg-gray-100">
+              <td class="py-3 px-6 text-left">
+                <div class="flex items-center">
+                  <div class="mr-2">
+                    <img
+                      class="w-6 h-6"
+                      src="https://img.icons8.com/cotton/64/000000/clock--v1.png"
+                      alt=""
+                    />
+                  </div>
+                  <span class="font-medium">Time...</span>
+                </div>
+              </td>
+
+              <td class="py-3 px-6 text-center">
+                <span class="bg-green-200 text-green-600 py-1 px-3 rounded-full"
+                  >(Selecting leader, response from leader, sending requests,
+                  etc...)</span
+                >
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
